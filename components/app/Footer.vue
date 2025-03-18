39b59d8992701fb700e2093c67af14ae33be8487
MostafaNegm222 <mostafanegm0002@gmail.com>
2025-03-18 02:37:03 +0200

    first edtion of navbar

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
       <div class="bg-green-900 text-white text-center p-4 font-[inter] md:flex flex-col items-center hidden">
-        <h2 class="lg:text-5xl md:text-3xl text-3xl mb-6 font-inter font-normal">Zaytona</h2>
+        <nuxt-link to="/" class="lg:text-5xl md:text-3xl text-3xl mb-6 font-inter font-normal">Zaytona</nuxt-link>
         <div class="md:flex md:items-center lg:gap-6 gap-3 lg:mb-12 mb-6 font-playfair lg:text-2xl md:text-xl font-normal grid grid-cols-3 ">
             <nuxt-link to="/">Home</nuxt-link>
             <nuxt-link to="/user/AboutUs">Our Stroy</nuxt-link>
@@ -13,11 +13,11 @@
         </div>
         <div class="md:flex md:items-center md:gap-20 lg:mb-8 mb-4 font-playfair lg:text-xl md:text-lg font-normal grid grid-col-2 gap-3">
             <p>Phone Number : <a href="tel:+201005066611">+20 100 506 6611</a></p>
-            <a href="https://www.google.com" class=" underline underline-offset-5">Location Link</a>
-            <p>Email : <a href="mailto:zaytona@gmail.com">zaytona@gmail.com</a></p>
+            <a href="https://www.google.com/maps/place/Information+Technology+Institute,Ismailia/@30.6087768,32.2494795,14z/data=!4m10!1m2!2m1!1siti+ismailia!3m6!1s0x14f8597f2f81ea4b:0xbb2533d2df13bd74!8m2!3d30.620847!4d32.2704842!15sCgxpdGkgaXNtYWlsaWGSARJyZXNlYXJjaF9pbnN0aXR1dGXgAQA!16s%2Fg%2F11c1w0d6ms?entry=ttu&g_ep=EgoyMDI1MDMxMi4wIKXMDSoASAFQAw%3D%3D" class=" underline underline-offset-5">Location Link</a>
+            <p class=" cursor-default">Email : <a href="mailto:zaytona@gmail.com">zaytona@gmail.com</a></p>
         </div>
         <div class="flex gap-12 lg:mb-6 mb-3 font-playfair lg:text-xl md:text-lg font-normal">
-            <p>Open daily from 08:00 AM to 12:00 Am</p>
+            <p class=" cursor-default">Open daily from 08:00 AM to 12:00 Am</p>
             <nuxt-link to="/auth/login">Sign up to get notified with special offers and events.</nuxt-link>
         </div>
         <div class="social flex gap-8 lg:mb-8 mb-4">
@@ -25,10 +25,10 @@
             <Icon name="uil:instagram" class="text-white w-6 h-6 " />
             <Icon name="uil:music" class="text-white w-6 h-6 " />
         </div>
-        <p class=" lg:text-xl md:text-lg">&copy; {{ new Date().getFullYear() }} Zaytona. All rights reserved.</p>
+        <p class=" lg:text-xl md:text-lg cursor-default">&copy; {{ new Date().getFullYear() }} Zaytona. All rights reserved.</p>
       </div>
       <div class="bg-green-900 text-white text-center p-4 font-inter block md:hidden">
-        <h2 class="lg:text-5xl md:text-3xl text-3xl mb-6 font-inter font-normal">Zaytona</h2>
+        <nuxt-link to="/" class="lg:text-5xl md:text-3xl text-3xl mb-6 font-inter font-normal">Zaytona</nuxt-link>
         <p class=" lg:text-xl md:text-lg">&copy; {{ new Date().getFullYear() }} Zaytona. All rights reserved.</p>
       </div>
     </footer>
--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,3 +1,9 @@
+<script setup>
+    definePageMeta({
+        layout : false
+    })
+</script>
+
 <template>
     <h1>Login</h1>
 </template>
