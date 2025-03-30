678eba8808fd027e39c26bfb364493c11c0a3579
NesmaHossam <nh3643769@gmail.com>
2025-03-30 05:07:59 +0200

    drop down menu

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
 const auth = useAuth();
 const isMenuOpen = ref(false);
@@ -6,17 +6,40 @@ const isMenuOpen = ref(false);
 // const userId = user.userObj.user.id
 // console.log(userId);
 
+//dropdown menu
+interface DropdownMenuItem {
+  label: string;
+  icon: string;
+  to?: string;
+  onSelect?: () => void;
+}
+
+const items = ref<DropdownMenuItem[]>([
+  {
+    label: "Profile",
+    icon: "i-lucide-user",
+    to: "/user/profile",
+  },
+  {
+    label: "Logout",
+    icon: "i-lucide-log-out",
+    // to: '#',
+    onSelect: () => {
+      auth.logout();
+    },
+  },
+]);
 </script>
 
 <template>
   <header
     v-if="!isMenuOpen"
-    class="bg-primary text-playfair text-xl text-amber-50 fixed top-0 left-0 right-0 z-1 py-4 "
+    class="bg-primary text-playfair text-xl text-amber-50 fixed top-0 left-0 right-0 z-1 py-4"
   >
     <UContainer class="flex justify-between items-center">
       <div>
         <nuxt-link to="/">
-          <img src="../../assets/images/Logo.png" alt="logo image"  >
+          <img src="../../assets/images/Logo.png" alt="logo image" />
         </nuxt-link>
       </div>
 
@@ -32,24 +55,44 @@ const isMenuOpen = ref(false);
         <nuxt-link to="/user/ContactUs" class="navigate relative"
           >Contact Us</nuxt-link
         >
-        <nuxt-link :to="`/user/profile`" class="navigate relative"
-          >Profile</nuxt-link
-        >
       </div>
 
       <div class="hidden md:block">
         <appAuthChecker>
           <template #auth>
-            <nuxt-link
+            <!-- <nuxt-link
               to="#"
               class="underline underline-offset-4"
               @click="auth.logout"
             >
               Log Out
-            </nuxt-link>
+            </nuxt-link> -->
+
+            <UDropdownMenu
+              size="xl"
+              :items="items"
+              :content="{
+                align: 'start',
+              }"
+              :ui="{
+                content: 'w-48 ',
+              }"
+            >
+              <UButton
+                size="xl"
+                label="UserName"
+                icon="i-lucide-user"
+                variant="outline"
+                class="bg-white text-primary cursor-pointer hover:bg-white"
+              />
+            </UDropdownMenu>
+          <NuxtLink to="/user/Cart"><UIcon name="i-heroicons-shopping-cart" class="w-8 h-8  ml-2" /></NuxtLink>  
           </template>
           <template #unAuth>
-            <nuxt-link to="/auth/Login" class="relative text-primary bg-white px-4 py-2 border border-white rounded-2xl">
+            <nuxt-link
+              to="/auth/Login"
+              class="relative text-primary bg-white px-4 py-2 border border-white rounded-2xl"
+            >
               Sign in
             </nuxt-link>
           </template>
@@ -60,7 +103,7 @@ const isMenuOpen = ref(false);
         class="md:hidden focus:outline-none"
         @click="isMenuOpen = !isMenuOpen"
       >
-        <span class="text-3xl ">&#9776;</span>
+        <span class="text-3xl">&#9776;</span>
       </button>
     </UContainer>
   </header>
@@ -81,7 +124,7 @@ const isMenuOpen = ref(false);
         src="../../assets/images/Logo.png"
         alt="logo image"
         class="w-[60%] md:w-[100%] max-w-[150px] mb-6"
-      >
+      />
 
       <nuxt-link to="/" class="py-4 text-xl" @click="isMenuOpen = false"
         >Home</nuxt-link
@@ -150,9 +193,9 @@ const isMenuOpen = ref(false);
   left: 0;
 }
 
-
 @media (min-width: 768px) and (max-width: 1180px) {
   .nav {
-font-size:17px ; }
+    font-size: 17px;
+  }
 }
 </style>
--- components/profile/Reservation.vue
+++ components/profile/Reservation.vue
@@ -1,137 +1,129 @@
 <script setup>
-import { ref } from "vue";
+const open = ref(false);
 
 const reservations = ref([
   {
     id: 1,
-    guestName: "Rahma Mandouh",
+    userName: "Rahma Mandouh",
     status: "Pending",
     reservationDate: "18/04/2025",
     mealType: "Breakfast",
     reservationTime: "08:00 AM",
     tableSetting: "Outdoor",
     tableFor: "4 Guests",
-
-    showCancelPopup: false,
   },
   {
     id: 2,
-    guestName: "Nesma Hossam",
+    userName: "Nesma Hossam",
     status: "Completed",
-    deposit: "Paid",
-    reservationCode: "050501",
     reservationDate: "15/03/2025",
     mealType: "Dinner",
     reservationTime: "05:00 PM",
     tableSetting: "Indoor",
     tableFor: "4 Guests",
-    selectedMeals: [
-      { name: "Grilled Chicken", quantity: "4 Dishes" },
-      { name: "Pink Pasta", quantity: "4 Dishes" },
-      { name: "Fresh Lemon Juice", quantity: "4 Drinks" },
-    ],
-    showCancelPopup: false,
   },
 ]);
 </script>
 
 <template>
-  <div class="flex gap-8">
-    <USeparator
-      color="primary"
-      type="solid"
-      orientation="vertical"
-      class="h-auto hidden md:block"
-    />
-    <UContainer>
-
-      <div class="flex flex-col w-full">
-        <ProfileWrapper
-          v-for="reservation in reservations"
-          :key="reservation.id"
-          class="flex flex-col rounded-lg mb-4"
-        >
-          <h2 class="text-xl md:text-2xl font-bold text-center">
-            Reservation Summary
-          </h2>
-          <h4 class="text-center my-2">Guest Name</h4>
-          <h3 class="text-center font-bold text-lg md:text-xl">
-            {{ reservation.guestName }}
-          </h3>
-          <div class="my-4">
-            <div class="flex justify-between items-center">
-              <h2 class="font-bold md:text-lg">Status:</h2>
-              <span
-                :class="
-                  reservation.status === 'Pending'
-                    ? 'text-red-500 font-bold'
-                    : 'text-green-500 font-bold'
-                "
-                >{{ reservation.status }}</span
-              >
-            </div>
-  
-            <div class="flex justify-between items-center">
-              <h2 class="font-bold md:text-lg">Reservation Date:</h2>
-              <span>{{ reservation.reservationDate }}</span>
-            </div>
-            <div class="flex justify-between items-center">
-              <h2 class="font-bold md:text-lg">Meal Type:</h2>
-              <span>{{ reservation.mealType }}</span>
-            </div>
-            <div class="flex justify-between items-center">
-              <h2 class="font-bold md:text-lg">Reservation Time:</h2>
-              <span>{{ reservation.reservationTime }}</span>
-            </div>
-            <div class="flex justify-between items-center">
-              <h2 class="font-bold md:text-lg">Table Setting:</h2>
-              <span>{{ reservation.tableSetting }}</span>
-            </div>
-            <div class="flex justify-between items-center">
-              <h2 class="font-bold md:text-lg">Table For:</h2>
-              <span>{{ reservation.tableFor }}</span>
-            </div>
-          </div>
-  
-          <button
-            v-if="reservation.status === 'Pending'"
-            @click="reservation.showCancelPopup = true"
-            class="bg-primary text-white px-4 py-2 mt-5 rounded cursor-pointer mx-auto block"
-          >
-            Cancel Reservation
-          </button>
-  
-          <div
-            v-if="reservation.showCancelPopup"
-            class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50"
-          >
-            <div
-              class="bg-slate-200/10 backdrop-blur-md shadow-lg p-6 lg:p-12 overflow-hidden relative rounded-tl-[30%] rounded-br-[30%] border border-slate-100 w-[80%] flex flex-col items-center"
-            >
-              <h2 class="text-xl font-bold text-center">Reservation Summary</h2>
-              <h4 class="text-center">Guest Name</h4>
-              <h3 class="text-center font-bold">{{ reservation.guestName }}</h3>
-              <p>
-                Are you sure you want to cancel your reservation with code
-                {{ reservation.reservationCode }}?
-              </p>
-              <div class="flex justify-end mt-4">
-                <button
-                  @click="reservation.showCancelPopup = false"
-                  class="mr-2 px-4 py-2 bg-primary text-white rounded cursor-pointer"
-                >
-                  No, keep this reservation
-                </button>
-                <button
-                  class="px-4 py-2 bg-red-500 text-white rounded cursor-pointer"
-                >
-                  Yes, cancel this reservation
-                </button>
-              </div>
-            </div>
-          </div>
-        </ProfileWrapper>
-      </div>
-    </UContainer>
+  <div class="mb-6 ms-0 md:ms-14 flex justify-between">
+    <h2 class="text-xl md:text-3xl font-semibold">Reservations</h2>
+    <UButton class="md:text-xl text-sm"
+      ><nuxt-link to="/user/tables">Reserve Table </nuxt-link></UButton
+    >
   </div>
+
+  <ProfileWrapper
+    v-for="(reservation ,index) in reservations"
+    :key="reservation.id"
+    class="flex flex-col rounded-lg mb-4"
+  >
+    <h2 class="text-xl md:text-2xl font-bold text-center">
+      Reservation Summary
+    </h2>
+    <h4 class="text-center my-2">Guest Name</h4>
+    <h3 class="text-center font-bold text-lg md:text-xl">
+      {{ reservation.userName }}
+    </h3>
+    <div class="my-4 w-[80%]">
+      <div class="flex justify-between items-center">
+        <h2 class="font-bold md:text-lg">Status:</h2>
+        <span
+          :class="
+            reservation.status === 'Pending'
+              ? 'text-red-500 font-bold md:text-lg'
+              : 'text-green-500 font-bold md:text-lg'
+          "
+          >{{ reservation.status }}</span
+        >
+      </div>
+
+      <div class="flex justify-between items-center">
+        <h2 class="font-bold md:text-lg">Reservation Date:</h2>
+        <span class="md:text-lg">{{ reservation.reservationDate }}</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <h2 class="font-bold md:text-lg">Meal Type:</h2>
+        <span class="md:text-lg">{{ reservation.mealType }}</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <h2 class="font-bold md:text-lg">Reservation Time:</h2>
+        <span class="md:text-lg">{{ reservation.reservationTime }}</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <h2 class="font-bold md:text-lg">Table Setting:</h2>
+        <span class="md:text-lg">{{ reservation.tableSetting }}</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <h2 class="font-bold md:text-lg">Table For:</h2>
+        <span class="md:text-lg">{{ reservation.tableFor }}</span>
+      </div>
+    </div>
+
+    <UModal
+      v-model:open="open"
+      :ui="{
+        content:
+          'bg-transparent border-0 shadow-none rounded-none divided-none ring-transparent ',
+      }"
+    >
+      <UButton
+        @click="reservation.showModal = true"
+        label="Open"
+        color="neutral"
+        variant="subtle"
+        v-if="reservation.status === 'Pending'"
+        class="bg-primary text-white px-4 py-2 mt-5 rounded cursor-pointer mx-auto block hover:bg-primary"
+      >
+        Cancel Reservation
+      </UButton>
+
+      <template #content>
+        <div
+          class="bg-slate-400/10 backdrop-blur-md shadow-md p-6 lg:p-12 overflow-hidden relative rounded-tl-[30%] rounded-br-[30%] border border-slate-300 w-full flex flex-col items-center"
+        >
+          <h2 class="text-xl md:text-2xl font-bold text-center">Reservation Summary</h2>
+          <h4 class="text-center text-lg">Guest Name</h4>
+          <h3 class="text-center font-bold text-xl md:text-2xl mb-2">{{ reservation.userName }}</h3>
+          <p class="text-center text-xl">
+            Are you sure about canceling your reservation?
+          </p>
+
+          <div class="flex justify-end mt-5 gap-4">
+            <UButton
+              class="mr-2 px-2 py-1 md:px-4 md:py-2 text-sm bg-primary text-white rounded cursor-pointer"
+              @click="open = false"
+            >
+              No, keep this reservation
+            </UButton>
+            <UButton
+              class="px-2 py-1 md:px-4 md:py-2 text-sm bg-red-500 text-white rounded cursor-pointer"
+            >
+              Yes, cancel this reservation
+            </UButton>
+          </div>
+        </div>
+      </template>
+    </UModal>
+  </ProfileWrapper>
 </template>
